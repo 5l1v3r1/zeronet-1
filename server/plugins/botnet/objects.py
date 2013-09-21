597d51de93e175e897a10e0e3b15e5d694d2fa05
Russley Shaw <russleyshaw@gmail.com>
2013-09-21 18:43:27 -0500

    codegen rerun

--- server/plugins/botnet/objects.py
+++ server/plugins/botnet/objects.py
@@ -3,10 +3,36 @@ from .game import Game
 from  util import command
 from game_utils import takes, success, failure
 
+class Mappable(Game.Object):
+    _game_state_attributes = ['id', 'x', 'y']
+    _relations = {}
+    _remotes = {}
+
+    def __init__(self, game, **kwargs):
+        Game.Object.__init__(self, game, **kwargs)
+        #TODO: Fill in any work that needs to be done when an object is made
+        #Common example would be setting the unit's health to maximum
+
+    def before_turn(self):
+        #TODO: Fill in start of turn values
+        #Common example would be giving units moves before their turn
+        pass
+
+    def after_turn(self):
+        #TODO: Set post-turn values
+        #Common example would be zeroing unit moves after the turn
+        pass
+
+
 class Player(Game.Object):
     _game_state_attributes = ['id', 'name', 'byte_dollars', 'cycles', 'time']
     _relations = {}
     _remotes = {}
+
+    def __init__(self, game, **kwargs):
+        Game.Object.__init__(self, game, **kwargs)
+        #TODO: Fill in any work that needs to be done when an object is made
+        #Common example would be setting the unit's health to maximum
 
     def before_turn(self):
         #TODO: Fill in start of turn values
@@ -24,10 +50,15 @@ class Player(Game.Object):
         pass
 
 
-class Mappable(Game.Object):
-    _game_state_attributes = ['id', 'x', 'y']
+class Base(Game.Object):
+    _game_state_attributes = ['id', 'x', 'y', 'owner', 'spawns_left']
     _relations = {}
     _remotes = {}
+
+    def __init__(self, game, **kwargs):
+        Game.Object.__init__(self, game, **kwargs)
+        #TODO: Fill in any work that needs to be done when an object is made
+        #Common example would be setting the unit's health to maximum
 
     def before_turn(self):
         #TODO: Fill in start of turn values
@@ -39,11 +70,21 @@ class Mappable(Game.Object):
         #Common example would be zeroing unit moves after the turn
         pass
 
+    @command
+    @takes(level = int)
+    def spawn(self, level = None):
+        pass
+
 
 class Tile(Game.Object):
     _game_state_attributes = ['id', 'x', 'y', 'owner']
     _relations = {}
     _remotes = {}
+
+    def __init__(self, game, **kwargs):
+        Game.Object.__init__(self, game, **kwargs)
+        #TODO: Fill in any work that needs to be done when an object is made
+        #Common example would be setting the unit's health to maximum
 
     def before_turn(self):
         #TODO: Fill in start of turn values
@@ -61,6 +102,11 @@ class Virus(Game.Object):
     _relations = {}
     _remotes = {}
 
+    def __init__(self, game, **kwargs):
+        Game.Object.__init__(self, game, **kwargs)
+        #TODO: Fill in any work that needs to be done when an object is made
+        #Common example would be setting the unit's health to maximum
+
     def before_turn(self):
         #TODO: Fill in start of turn values
         #Common example would be giving units moves before their turn
@@ -74,27 +120,6 @@ class Virus(Game.Object):
     @command
     @takes(x = int, y = int)
     def move(self, x = None, y = None):
-        pass
-
-
-class Base(Game.Object):
-    _game_state_attributes = ['id', 'x', 'y', 'owner', 'spawns_left']
-    _relations = {}
-    _remotes = {}
-
-    def before_turn(self):
-        #TODO: Fill in start of turn values
-        #Common example would be giving units moves before their turn
-        pass
-
-    def after_turn(self):
-        #TODO: Set post-turn values
-        #Common example would be zeroing unit moves after the turn
-        pass
-
-    @command
-    @takes(level = int)
-    def spawn(self, level = None):
         pass
 
 
