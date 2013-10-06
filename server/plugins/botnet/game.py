c19b29742b92456e3e594e4da042606ffba24a2c
Russley Shaw <russleyshaw@gmail.com>
2013-10-06 16:51:45 -0500

    Added game logic. But...
    Error involving player.time not being avalible because player is of type
    none.

--- server/plugins/botnet/game.py
+++ server/plugins/botnet/game.py
@@ -1,5 +1,8 @@
 import game_objects
 import objects
+import math
+import time
+import os
 
 class Game(game_objects.Game):
     _name = 'botnet'
@@ -9,6 +12,139 @@ class Game(game_objects.Game):
     _relations = {}
     _remotes = {}
 
+    def __init__(self, game, **kwargs):
+        print('__INIT__ GAME')
+        game_objects.Game.__init__(self, game, **kwargs)
+        #TODO: Fill in any work that needs to be done when a match is made
+        #Common example would be creating the config
+
+        self.config = self.load_config('defaults')
+        self.turn_number = 0
+        self.game_number = 0
+        self.base_cost = self.config['globals']['base_cost']
+        self.scale_cost = self.config['globals']['scale_cost']
+        self.width = self.config['globals']['width']
+        self.height = self.config['globals']['height']
+        self.player_id = 0
+
+        print('END __INIT__')
+
+    def get_tile(self, x, y):
+        return self.tiles[x * self.mapHeight + y]
+
+    def worth(self, id):
+        # Calculate the worth of a player
+        total = self.objects.players[id].cycles
+        for virus in self.objects.viruses:
+            if virus.owner == id:
+                total += self.virus_cost(virus.level)
+        return total
+
+    def start_map (self):
+        map_filenames = []
+        #look through the list of map files, for every .map file, add to mapfilename list
+        for filename in os.listdir("maps/"):
+            if ".map" in filename:
+                map_filenames.append(filename)
+
+        #choose a random map, open it as f, then get rid of all whitespace in file, save that file as mapdata, close f
+        with open(("maps/" + map_filenames[int((time.time()*1000) % len(map_filenames))]),'r') as f:
+            mapdata = f.read().replace(' ','').split()
+        self.width = len(mapdata[0])
+        self.height = len(mapdata)
+        #Need to get the attributes for the game objects before we parse the file
+
+        #saves y data as a enumeration called row, iterates through
+        #does the same for x, saved as mapSquare. mapsquare points at map[x][y]
+        for y, row in enumerate(mapdata):
+            for x, map_square in enumerate(row):
+                #if mapSquare is an X then it is a wall, owned by 3
+                if map_square == 'X':
+                    #self.grid[x][y] = self.addObject(objects.Tile, [x,y,3])
+                    self.grid[x][y].owner = 3
+                #if mapSquare is a . then it is a neutral tile owned by 2
+                elif map_square == '.':
+                    self.grid[x][y] = self.add_object(objects.Tile, [x,y,2])
+                #if mapSquare is 1, then it is a tile owned by player 1, which means
+                #there's a base on top, so we add a base too
+                elif map_square == '1':
+                    self.grid[x][y] = self.addObject(objects.Tile, [x,y,1])
+                    self.add_object(game_objects.Base,[x, y, 1, 0])
+                #same as previous, only it is player 0's base/tile combo
+                elif map_square =='0':
+                    self.grid[x][y] = self.addObject(objects.Tile, [x,y,0])
+                    self.add_object(game_objects.Base,[x, y, 0, 0])
+
+    def getScore(self, id):
+        path = []
+        connect = {}
+        closed = [[False]*self.height for _ in range(self.width)]
+        offsets = [(0,1),(1,0),(0,-1),(-1,0)]
+        score = 0
+        for base in self.objects.bases:
+            if base.owner == id:
+                score+=1
+                path.append(self.grid[base.x][base.y])
+                closed[base.x][base.y] = True
+        while len(path)>0:
+            working = path.pop()
+            for offset in offsets:
+                dx, dy = working.x+offset[0], working.y+offset[1]
+                if 0 <= dx < self.width and 0 <= dy < self.height and not closed[dx][dy] and self.grid[dx][dy].owner == id:
+                    score+=1
+                    path.append(self.grid[dx][dy])
+                    closed[dx][dy] = True
+        return score
+
+    def bigger_area(self):
+        p1, p2 = set(), set()
+        # Builds a dictionary of all controlled tiles for both players
+        for tile in self.objects.tiles:
+            if tile.owner == 0:
+                p1.add((tile.x, tile.y))
+            elif tile.owner == 1:
+                p2.add((tile.x, tile.y))
+        # defines adjacency
+        offsets = [(0,1), (1,0), (0,-1), (-1,0)]
+
+        def connect(openList, available):
+            # Given a list of starting points and a set of valid points, find how many are connected to the start
+            size = len(openList)
+            while len(openList) > 0:
+                working = openList.pop()
+                for offset in offsets:
+                    next = working[0] + offset[0], working[1] + offset[1]
+                    if next in available:
+                        available.remove(next)
+                        size += 1
+                        openList.append(next)
+            return size
+
+        def max_area(available):
+            # Given a dictionary of valid tiles
+            largest = 0
+            while len(available) > 0:
+                next = available.pop()
+                size = connect([next], available)
+                if largest < size:
+                    largest = size
+                return largest
+
+        p1size, p2size = max_area(p1), max_area(p2)
+        if p1size > p2size:
+          return 1, 0
+        elif p1size < p2size:
+          return 0, 1
+        else:
+          return 0, 0
+
+    def virus_cost(self, level):
+        return int(self.base_cost*self.scale_cost ** level)
+
+    def get_income(self, id):
+        possible = self.starting_cycles + self.turn_number / 2 * self.cycles_per_turn
+        return int(math.ceil((possible - self.worth(id))*self.return_amount) + self.cycles_per_turn)
+
     def before_start(self):
         #TODO: Initialize the game
 
@@ -16,13 +152,7 @@ class Game(game_objects.Game):
         #Initialize any global values
         #At this point Player objects exist
         #(But any game-specific values will be uninitialized)
-        config = self.load_config('defaults')
-        self.game_length = config['globals']['game_length']
-
-        self.turn_number = 0
-        self.player_id = 0
-        self.width = config['globals']['width']
-        self.height = config['globals']['height']
+        self.grid = [[[ self.add_object(objects.Tile,[x, y, 2]) ] for y in range(self.height)] for x in range(self.width)]
 
 
     def before_turn(self):
@@ -34,10 +164,8 @@ class Game(game_objects.Game):
         #Start of turn income
         #Creating units whose construction began previously
 
-        #self.turn_number += 1
-
         if self.turn_number % 2 == 0:
-            score = [10, 10]
+            score = self.bigger_area()
             self.players[0].byte_dollars += score[0]
             self.players[1].byte_dollars += score[1]
 
--- server/plugins/botnet/objects.py
+++ server/plugins/botnet/objects.py
@@ -33,10 +33,16 @@ class Player(Game.Object):
         Game.Object.__init__(self, game, **kwargs)
         #TODO: Fill in any work that needs to be done when an object is made
         #Common example would be setting the unit's health to maximum
+        self.name = ''
+        self.byte_dollars = self.game.config['default']['byte_dollars']
+        self.cycles = self.game.config['default']['cycles']
+        self.time = int(0)
 
     def before_turn(self):
         #TODO: Fill in start of turn values
         #Common example would be giving units moves before their turn
+        self.game.current_player = self
+        self.cycles += self.game.get_income(self.id)
         pass
 
     def after_turn(self):
@@ -80,21 +86,51 @@ class Virus(Game.Object):
         Game.Object.__init__(self, game, **kwargs)
         #TODO: Fill in any work that needs to be done when an object is made
         #Common example would be setting the unit's health to maximum
+        self.living = 1
 
     def before_turn(self):
         #TODO: Fill in start of turn values
         #Common example would be giving units moves before their turn
-        pass
+        self.moves_left = 1
+        return
 
     def after_turn(self):
         #TODO: Set post-turn values
         #Common example would be zeroing unit moves after the turn
-        pass
+        self.moves_left = 0
+        return
 
     @command
     @takes(x = int, y = int)
     def move(self, x = None, y = None):
-        pass
+        if self.owner != self.game.player_id:
+            return 'Turn {}: You cannot control a virus that is not yours'.format(self.game.turn_number)
+        elif self.moves_left <= 0:
+            return 'Turn {}: Your virus {} has no moves left. ({},{}) -> ({},{})'.format(self.game.turn_number, self.id, self.x, self.y, x, y)
+        elif not (0 <= x < self.game.width) or not(0 <= y < self.game.height):
+            return 'Turn {}: Your virus {} cannot move off the map. ({},{}) -> ({},{})'.format(self.game.turn_number, self.id, self.x, self.y, x, y)
+        elif abs(self.x - x) + abs(self.y - y) != 1:
+            return 'Turn {}: Your virus {} can only move to adjacent tiles. ({},{}) -> ({},{})'.format(self.game.turn_number, self.id, self.x, self.y, x, y)
+        elif self.game.grid[x][y][0].owner == 3:
+            return 'Turn {}: Your virus {} cannot move onto a wall. ({},{}) -> ({},{})'.format(self.game.turn_number, self.id, self.x, self.y, x, y)
+
+        #TODO Handle walking into bases
+        for base in self.bases:
+            pass
+
+        #TODO Handle walking into other viruses
+        for virus in self.viruses:
+            pass
+
+        self.game.grid[self.x][self.y].remove(self)
+        self.x = x
+        self.y = y
+        self.moves_left -= 1
+        self.game.grid[x][y].append(self)
+
+
+        return True
+
 
 
 class Base(Game.Object):
@@ -110,16 +146,38 @@ class Base(Game.Object):
     def before_turn(self):
         #TODO: Fill in start of turn values
         #Common example would be giving units moves before their turn
-        pass
+        self.spawns_left = 1
+        return
 
     def after_turn(self):
         #TODO: Set post-turn values
         #Common example would be zeroing unit moves after the turn
-        pass
+        self.spawns_left = 0
+        return
 
     @command
     @takes(level = int)
     def spawn(self, level = None):
-        pass
+        player = self.game.players[self.player_id]
+        cost = self.game.virus_cost(level)
+
+        if self.owner != self.game.player_id:
+            return 'Turn {}: You cannot spawn a virus on a base {} you do not own. ({},{})'.format(self.game.turn_number, self.id, self.x, self.y)
+        elif level < 0:
+            return 'Turn {}: You cannot spawn a virus with a level {}. ({},{})'.format(self.game.turn_number, level, self.x, self.y)
+        elif player.cycles < cost:
+            return 'Turn {}: You do not have enough cycles({}) to spawn this virus(level:{} cost:{}). ({},{})'.format(self.game.turn_number, player.cycles, level, cost, self.x, self.y)
+        elif self.spawns_left <= 0:
+            return 'Turn {}: You do not have any spawns left to spawn this virus(level:{}). ({},{}) '.format(self.game.turn_number, level, self.x, self.y)
+
+        player.cycles -= cost
+        self.spawns_left -= 1
+        newVirus = self.game.add_object(Game.Object.Virus,[self.x,self.y,self.owner,level,0])
+
+        return True
+
+
+
+
 
 
