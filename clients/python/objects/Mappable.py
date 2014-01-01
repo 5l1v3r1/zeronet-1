2d04686c6aa2f6efb250e8b90bd454ac8ad198f5
Russley Shaw <russleyshaw@gmail.com>
2014-01-01 16:12:06 -0600

    codegen rerun

---
+++ clients/python/objects/Mappable.py
@@ -0,0 +1,36 @@
+import operator
+import utility
+import json
+import client_json
+import game
+from game_object import GameObject
+from .Mappable import Mappable
+from .Mappable import Mappable
+from .Mappable import Mappable
+from .Mappable import Mappable
+
+## @class Mappable
+#  @brief The base object for all mappable things
+class Mappable(GameObject):
+
+    def __init__(self, connection, parent_game, id, x, y):
+        self._connection = connection
+        self._parent_game = parent_game
+        self._id = id
+        self._x = x
+        self._y = y
+
+
+    @property
+    def id(self):
+        return self.id
+    @property
+    def x(self):
+        return self.x
+    @property
+    def y(self):
+        return self.y
+
+
+
+
